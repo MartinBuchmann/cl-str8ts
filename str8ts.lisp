8ceae82fe953e2895285164614da41e13d0c2456
Martin Buchmann <Martin.Buchmann@googlemail.com>
2019-01-28 20:39:54 +0100

    Changed the way of encoding a puzzle.

--- str8ts.lisp
+++ str8ts.lisp
@@ -1,4 +1,4 @@
-;; Time-stamp: <2019-01-27 23:03:21 Martin>
+;; Time-stamp: <2019-01-28 20:38:40 Martin>
 ;; * str8ts.lisp
 ;; See Readme.org for more information
 ;;
@@ -9,25 +9,32 @@
 (prove:plan 2)
 
 ;; * Defining the data structures
+
+(deftype candidate ()
+  "A new type of the possible values of a puzzle field."
+  '(integer -9 9))
+
+;; ** Testing the new types
+(prove:ok (typep 4 'candidate))
+(prove:ok (not (typep -10 'candidate)))
+
+;; TODO: Think about an nine digit bit-vector indicating the possible candidates
 ;;
 ;; I don't know what is the best data structure yet but will start with an
-;; 3D-array holding the lines, columns and in the third dimension the fixed
-;; values.
-(defvar *the-puzzle* (make-array (list 9 9 2)))
-;; TODO: Decide which element type fits best.
+;; 2D-array holding the lines and columns.
+(defvar *the-puzzle* (make-array (list 9 9) :element-type 'candidate))
 
 ;; * Read in a puzzle
 (defun read-puzzle (&optional (file #p"medium.txt"))
-  "Read in the puzzle's data where each puzzle consists of two lines:
+  "Read in the puzzle's data where each puzzle consists of one line:
 
-- The first line indicates the black fields (X), the given numbers and the empty fields (.) 
-- The second line consists of the blocked numbers in the black fiels and open fields (.)
+A zero indicates a black field, negative numbers are numbers within black fields,
+the given numbers and the empty fields are represented by \".\"
 "
   (with-open-file (puzzle file)
     (read-line puzzle)))
 
-;; TODO: Read in both lines
-;; TODO: Implement a simple print-puzzlr function
+;; TODO: Implement a simple print-puzzle function
 
 ;; * The predicate(s)
 
